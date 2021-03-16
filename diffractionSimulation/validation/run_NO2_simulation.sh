2201c03280321fcf982d1792f932fb939c94e82d
khegazy <kareemhegz@gmail.com>
2021-03-15 18:26:49 -0700

    Changed time ind to eval time.

--- diffractionSimulation/validation/run_NO2_simulation.sh
+++ diffractionSimulation/validation/run_NO2_simulation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  TIND=nan
+  TIME=nan
 else
-  TIND=${1}
+  TIME=${1}
 fi
 
-python3 ~/simulation/diffractionSimulation/diffraction.py --run "sim_validation" --molName "NO2" --xyz_file "NO2.xyz" --basis_folder "/cds/group/ued/scratch/N2O/axis_distributions/NO2/A/temp-100K" --time_ind ${TIND}
+python3 ../diffraction.py --run "sim_validation" --molecule "NO2" --xyz_file "NO2.xyz" --basis_folder "/cds/group/ued/scratch/N2O/axis_distributions/NO2/A/temp-100K" --eval_time ${TIME}
