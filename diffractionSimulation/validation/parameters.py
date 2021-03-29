691228ba9b022e30a269ac13ae02901344c1f3e9
khegazy <kareemhegz@gmail.com>
2021-03-29 14:22:37 -0700

    Updated analysis

--- diffractionSimulation/validation/parameters.py
+++ diffractionSimulation/validation/parameters.py
@@ -31,15 +31,15 @@ def get_parameters(run, molecule="N2O"):
         self.molecule  = "CF2IBr"
         self.xyz_file = "CF2IBr.xyz"
         self.NradAzmBins = 75
-        self.QperPix = 15.0/(self.NradAzmBins-1)
+        self.QperPix = 20.0/(self.NradAzmBins-1)
         N = 1
         self.sim_eval_times = np.arange(N)
         #self.sim_times = np.arange(5)*0.25 + 318
         self.calculation_type = "analytic"
         self.xyz_file = os.path.join(self.xyzDir, self.xyz_file)
         self.basis_folder = os.path.join(self.axisDistDir, "A/temp-100K")
-        self.smear_polar_bins = 5 
-        self.smear_azim_bins  = 5
-        self.smear_spin_bins  = 5
+        self.smear_polar_bins = 25 
+        self.smear_azim_bins  = 25
+        self.smear_spin_bins  = 25
 
   return parameters(run, molecule)
--- diffractionSimulation/validation/submitClusterJobs.sh
+++ diffractionSimulation/validation/submitClusterJobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-OUTDIR=/cds/group/ued/scratch/N2O/simulations/
+OUTDIR=./output/
 
 if [ -z "$2" ]; then
   echo "ERROR SUBMITTING JOBS!!!   Must give the name of the molecule and largest L value to submit!"
@@ -18,7 +18,7 @@ do
   do
     lmk=$j",0,"$k
     echo "Submitting "${lmk}
-    sbatch -p psanaq -o ${OUTPUTDIR}"job_"${lmk}".log" --wrap="python3 ~/simulation/diffractionSimulation/diffraction.py --run sim_validation --molecule CF2IBr --LMK $lmk --xyz_file CF2IBr.xyz --basis_folder /cds/group/ued/scratch/N2O/axis_distributions/NO2/A/temp-100K --time_ind 0 --cluster True"
+    sbatch -p psanaq -o ${OUTPUTDIR}"job_"${lmk}".log" --wrap="python3 ../diffraction.py --run sim_validation --molecule CF2IBr --LMK $lmk --xyz_file CF2IBr.xyz --basis_folder /cds/group/ued/scratch/N2O/axis_distributions/NO2/A/temp-100K --time_ind 0 --cluster True"
     #sbatch -p psanaq --DefaultTime=INFINITE -t 10-10:00 -o ${OUTPUTDIR}"job_"${lmk}".log" --wrap="python3 ~/simulation/diffractionSimulation/diffraction.py --run sim_validation --molecule CF2IBr --LMK $lmk --xyz_file CF2IBr.xyz --basis_folder /cds/group/ued/scratch/N2O/axis_distributions/NO2/A/temp-100K --time_ind 0 --cluster True"
   done
 done
